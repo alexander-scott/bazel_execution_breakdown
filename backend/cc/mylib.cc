ac60dade4acf1d314184fc0ad5267456d8863a74
Alex Scott <alexanderscott@outlook.com>
2026-06-27 07:35:16 +0000

    Add CC toolchain

---
+++ backend/cc/mylib.cc
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <fmt/core.h>
+#include "mylib.h"
+
+namespace mylib {
+
+int add(int a, int b) { return a + b; }
+
+std::string greet(const std::string &name) {
+  return fmt::format("Hello, {}!", name);
+}
+
+} // namespace mylib
